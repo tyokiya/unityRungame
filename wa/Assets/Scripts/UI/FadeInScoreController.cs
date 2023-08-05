3fd12b808c86077bc9a36bf2fc4e8e9c2ed1f9ea
tyokiya <tokiya.syouta@icloud.com>
2023-08-06 02:46:55 +0900

    フォント変更　スコアのフェードイン処理

---
+++ wa/Assets/Scripts/UI/FadeInScoreController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+////////////////////////////////////
+// アイテムスコア50ごとにフェードインするスコアコントローラー
+////////////////////////////////////
+public class FadeInScoreController : MonoBehaviour
+{
+    //インスペクターから設定
+    //子オブジェクトのスコアオブジェクトを入れる
+    [SerializeField] Text child_score_object;
+
+    //フェードインスピード
+    float fadeIn_speed = -3.0f;
+    //フェードインフラグ
+    bool fadeInFlg = false;
+    //フェードインの回数カウンター
+    int fadeIn_counter = 0;
+    //カウンターのマックス数
+    int counsterMax_num = 50;
+
+    void Update()
+    {
+        //フラグに応じてフェードイン処理
+        if (this.fadeInFlg == true)
+        {
+            //y軸移動
+            transform.Translate(0, this.fadeIn_speed, 0);
+            //カウンター増加
+            this.fadeIn_counter++;
+        }
+        //カウンター数がマックスまで達したらフラグを下ろし初期値へ移動
+        if (this.fadeIn_counter == this.counsterMax_num)
+        {
+            //初期値へ移動
+            transform.Translate(0, 170, 0);
+            //フラグを下ろす
+            this.fadeInFlg = false;
+            //カウンター初期化
+            this.fadeIn_counter = 0;
+        }
+    }
+
+    /// <summary>
+    /// スコアのフェードイン処理
+    /// </summary>
+    /// <param name="displayScore">表示するスコア</param>
+    public void fadeIn_itemScore(int displayScore)
+    {
+        //フェードインフラグを立てる
+        this.fadeInFlg = true;
+        //表示するスコアを更新
+        this.child_score_object.text = displayScore.ToString();
+    }
+}
--- wa/Assets/Scripts/UI/UIManager.cs
+++ wa/Assets/Scripts/UI/UIManager.cs
@@ -11,6 +11,8 @@ public class UIManager : MonoBehaviour
     [SerializeField] ScoreUIController scoreUIController_object;
     //アイテムUI管理オブジェクト
     [SerializeField] ItemUIController itemUIController_object;
+    //フェードインスコアのコントローラーオブジェクト
+    [SerializeField] FadeInScoreController fadeinScoreController_object;
 
     void Update()
     {
@@ -18,5 +20,12 @@ public class UIManager : MonoBehaviour
         this.scoreUIController_object.ScoreTextUpdate(this.scoreController_object.SucoreGetter());
         //アイテムUIの更新命令
         this.itemUIController_object.ItemTextUpdate(this.scoreController_object.ItemNumGetter());
+
+        //獲得数50ごとにフェードイン処理命令
+        if (this.scoreController_object.ItemNumGetter() == 50)
+        {
+            //フェードイン処理命令
+            this.fadeinScoreController_object.fadeIn_itemScore(this.scoreController_object.ItemNumGetter());
+        }
     }
 }
