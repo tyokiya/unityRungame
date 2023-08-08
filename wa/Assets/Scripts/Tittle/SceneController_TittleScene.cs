149153a037b8e43637d7164d6d2f9c75a2f9906c
tyokiya <tokiya.syouta@icloud.com>
2023-08-08 23:21:29 +0900

    各シーンの推移処理

---
+++ wa/Assets/Scripts/Tittle/SceneController_TittleScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController_TittleScene : MonoBehaviour
+{
+    /// <summary>
+    /// ゲームシーンへ切り替え
+    /// </summary>
+    public void ChangeScene_Game()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    /// <summary>
+    /// クレジットシーンへの切り替え
+    /// </summary>
+    public void ChangeScene_Credit()
+    {
+        SceneManager.LoadScene("CreditScene");
+    }
+
+    /// <summary>
+    /// チュートリアルシーンへの切り替え
+    /// </summary>
+    public void ChangeScene_Tutorial()
+    {
+        SceneManager.LoadScene("TutorialScene");
+    }
+}
