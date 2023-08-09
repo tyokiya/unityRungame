7d6cefd6d4803a4f2571dd97d20b9f05482dbfd8
tyokiya <tokiya.syouta@icloud.com>
2023-08-09 19:08:55 +0900

    クレジットシーン、タイトルシーンの完成

---
+++ wa/Assets/Scripts/Credit/SceneController_CreditScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+////////////////////////////////////
+// クレジットシーンのシーンコントローラー
+////////////////////////////////////
+
+public class SceneController_CreditScene : MonoBehaviour
+{
+    /// <summary>
+    /// タイトルシーンへ切り替え
+    /// </summary>
+    public void ChangeScene_Tittle()
+    {
+        SceneManager.LoadScene("TittleScene");
+    }
+}
--- wa/Assets/Scripts/Tittle/SceneController_TittleScene.cs
+++ wa/Assets/Scripts/Tittle/SceneController_TittleScene.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+////////////////////////////////////
+// タイトルシーンのシーンコントローラー
+////////////////////////////////////
+
 public class SceneController_TittleScene : MonoBehaviour
 {
     /// <summary>
